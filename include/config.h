378e4adaac0814f3a2485a8b51ed05d628c296e0
yorickdewid <yorick17@outlook.com>
2015-02-08 23:09:16 +0100

    update version

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H_INCLUDED
 #define CONFIG_H_INCLUDED
 
-#define DBVERSION	"0.0.3"
-#define IDXVERSION	"0.0.3"
+#define DBVERSION	"0.0.5"
+#define IDXVERSION	"0.0.5"
 
 #endif // CONFIG_H_INCLUDED
