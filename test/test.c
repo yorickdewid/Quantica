f4175e0701bc6acb2e8178965313b1e4333956c3
Yorickdewid <yorick17@outlook.com>
2015-02-21 22:50:59 +0100

    Remove env from test

--- test/test.c
+++ test/test.c
@@ -3,12 +3,6 @@
 
 int main(int argc, char *argv[], char *envp[]) {
 
-	char** env;
-	for(env = envp; *env != 0; env++){
-		char* thisEnv = *env;
-		printf("%s\n", thisEnv);
-	  }
-
 	CALL_BENCHMARK(performance);
 
 	return 0;
