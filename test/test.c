c168c6698b628bcb30c8bcf4788665199c60caaa
Yorickdewid <yorick17@outlook.com>
2015-02-21 21:19:42 +0100

    Print env on test

--- test/test.c
+++ test/test.c
@@ -1,7 +1,13 @@
 #include "test.h"
 #include "test-list.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, char **envp) {
+
+	char** env;
+	for(env = envp; *env != 0; env++){
+		char* thisEnv = *env;
+		printf("%s\n", thisEnv);
+	  }
 
 	CALL_BENCHMARK(performance);
 
