af925bb076b8dc9e923f79f7a6689f8d2fb8f096
yorickdewid <yorick17@outlook.com>
2015-03-24 13:24:25 +0100

    Fix uptime counter

--- src/core.c
+++ src/core.c
@@ -42,9 +42,12 @@ char *get_instance_name() {
 char *get_uptime(char *buf, size_t len) {
     qtime_t passed = get_timestamp()-uptime;
     unsigned int days = passed/86400;
+    passed = passed % 86400;
     unsigned int hours = passed/3600;
+    passed = passed % 3600;
     unsigned int mins = passed/60;
-    unsigned int secs = passed % 60;
+    passed = passed % 60;
+    unsigned int secs = passed;
     snprintf(buf, len, "%u days, %.2u:%.2u:%.2u", days, hours, mins, secs);
     return buf;
 }
