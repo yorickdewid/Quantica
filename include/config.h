3fe5f1c3888a84cb1b46133c5383c53589082b6d
yorickdewid <yorick17@outlook.com>
2015-04-27 23:57:48 +0200

    Release 0.3-beta

--- include/config.h
+++ include/config.h
@@ -4,8 +4,8 @@
 #define PROGNAME		"Quantica"
 #define INSTANCE		"SERVER01"
 #define VERSION_RELESE	0
-#define VERSION_MAJOR	2
-#define VERSION_MINOR	8
+#define VERSION_MAJOR	3
+#define VERSION_MINOR	0
 #define LOGFILE			"quantica.log"
 #define INITDB			"i1"
 
