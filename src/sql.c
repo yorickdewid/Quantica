7cdce17a8ce4ee1302d786db49664828cd66e6e8
yorickdewid <yorick17@outlook.com>
2015-06-17 23:14:47 +0200

    SQL tokenizer using stack

--- src/sql.c
+++ src/sql.c
@@ -5,6 +5,7 @@
 
 #include <config.h>
 #include <common.h>
+#include "stack.h"
 #include "sql.h"
 
 #define STACK_SZ	15
@@ -43,23 +44,10 @@ struct stoken {
 	} u;
 };
 
-typedef struct {
-	void **buf;
-	unsigned int size;
-} stack_t;
-
-stack_t tokenstream;
-
-void stack_push(struct stoken *token) {
-	if (tokenstream.size == STACK_SZ)
-		tokenstream.buf = realloc(tokenstream.buf, (2*tokenstream.size) * sizeof(void *));
-	tokenstream.buf[tokenstream.size++] = token;
-}
-
-void parse() {
-	unsigned int i;
-	for (i=0; i<tokenstream.size; ++i) {
-		struct stoken *tok = tokenstream.buf[i];
+void parse(stack_t *stack) {
+	int i;
+	for (i=0; i<=stack->top; ++i) {
+		struct stoken *tok = stack->contents[i];
 		if (tok->token == T_STRING)
 			printf("[%d] %d (%s)\n", i, tok->token, tok->u.string);
 		else if (tok->token == T_INTEGER)
@@ -178,7 +166,7 @@ char *explode_sql(char *sql) {
 	return _osql;
 }
 
-int tokenize(char sql[]) {
+int tokenize(stack_t *stack, char sql[]) {
 	char *_ustr = explode_sql(sql);
 	puts(_ustr);
 	char *pch = strtok(_ustr," ,");
@@ -186,79 +174,79 @@ int tokenize(char sql[]) {
 		if (!strcmp(pch, "SELECT")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_SELECT;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "INSERT")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_INSERT;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "INTO")) {
 		} else if (!strcmp(pch, "VALUES")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_SEPARATE;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "(")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_BRACK_OPEN;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, ")")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_BRACK_CLOSE;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "*")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_ALL;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "FROM")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_FROM;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "WHERE")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_WHERE;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "AND")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_AND;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "OR")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_OR;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, ">")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_GREATER;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "<")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token = T_SMALLER;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, "=")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token =  T_EQUAL;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (!strcmp(pch, ";")) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token =  T_COMMIT;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (strisdigit(pch)) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token =  T_INTEGER;
 			tok->u.integer = atoi(pch);
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if (strismatch(pch, "1234567890.")) {
 			if (strccnt(pch, '.') == 1) {
 				if (pch[0] != '.' && pch[strlen(pch)-1] != '.') {
 					struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 					tok->token =  T_DOUBLE;
 					tok->u.dbl = atof(pch);
-					stack_push(tok);
+					stack_push(stack, tok);
 				}
 			}
 		} else if (strisualpha(pch)) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token =  T_STRING;
 			tok->u.string = pch;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else if ((pch[0] == '"' && pch[strlen(pch)-1] == '"') ||	(pch[0] == '\'' && pch[strlen(pch)-1] == '\'')) {
 			struct stoken *tok = (struct stoken *)malloc(sizeof(struct stoken));
 			tok->token =  T_STRING;
@@ -266,29 +254,19 @@ int tokenize(char sql[]) {
 			_s[strlen(pch)-1] = '\0';
 			_s++;
 			tok->u.string = _s;
-			stack_push(tok);
+			stack_push(stack, tok);
 		} else {
 			printf("Unexpected token '%s'\n", pch);
 			return 0;
 		}
-
 		pch = strtok(NULL, " ,");
 	}
 	return 1;
 }
 
-#if 0
-int main(int argc, char *argv[]) {
-	tokenstream.buf = malloc(STACK_SZ * sizeof(void *));
-	tokenstream.size = 0;
-	if (argc < 2) {
-		char sql[] = "INSERT INTO kaas_e (id,kaas,worst,'blub',\"arie\") VALUES (12,'blub','wurst',ja,nee)";
-		if (tokenize(sql))
-			parse();
-	} else
-		if (tokenize(argv[1]))
-			parse();
-
-	return 0;
+void sql_exec(const char *sql) {
+	stack_t tokenstream;
+	stack_init(&tokenstream, STACK_SZ);
+	if (tokenize(&tokenstream, (char *)sql))
+		parse(&tokenstream);
 }
-#endif
--- src/sql.h
+++ src/sql.h
@@ -1,6 +1,6 @@
 #ifndef SQL_H_INCLUDED
 #define SQL_H_INCLUDED
 
-
+void sql_exec(const char *sql);
 
 #endif // SQL_H_INCLUDED
