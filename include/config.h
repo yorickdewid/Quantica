ee4e6cf68ce97de0ac5ce94338af010ee0114d64
yorickdewid <yorick17@outlook.com>
2015-06-30 00:02:32 +0200

    Update version

--- include/config.h
+++ include/config.h
@@ -4,8 +4,8 @@
 #define PROGNAME		"Quantica"
 #define INSTANCE_PREFIX	"QUANTZ"
 #define VERSION_RELESE	0
-#define VERSION_MAJOR	4
-#define VERSION_MINOR	15
+#define VERSION_MAJOR	5
+#define VERSION_MINOR	0
 #define LOGFILE			"quantica.log"
 #define BINDATA			"QUBIN_DATA"
 
