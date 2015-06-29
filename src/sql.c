adb145f1a16ab2ba47572dc7ff3dfd3f87bc7a44
yorickdewid <yorick17@outlook.com>
2015-06-29 10:51:44 +0200

    Implement standard SQL tokens

--- src/sql.c
+++ src/sql.c
@@ -25,10 +25,20 @@ enum token {
 	T_INSERT,
 	T_DELETE,
 	T_UPDATE,
-	T_ALL,
+	T_ASTERISK,
 	T_FROM,
 	T_WHERE,
+	/* logical */
 	T_AND,
+	T_ANY,
+	T_ALL,
+	T_BETWEEN,
+	T_EXISTS,
+	T_IN,
+	T_LIKE,
+	T_UNIQUE,
+	T_NOT,
+	T_IS,
 	T_OR,
 	/* group */
 	T_BRACK_OPEN,
@@ -36,7 +46,12 @@ enum token {
 	T_SEPARATE,
 	T_COMMIT,
 	T_TARGET,
-	/* operations */
+	/* arithmetic */
+	T_ADD,
+	T_SUBTRACT,
+	T_DEVIDE,
+	T_MODULO,
+	/* comparison */
 	T_GREATER,
 	T_SMALLER,
 	T_EQUAL,
@@ -67,312 +82,285 @@ sqlresult_t *parse(stack_t *stack, size_t *len) {
 		return &rs;
 	}
 	struct stoken *tok = stack_rpop(stack);
-	switch (tok->token) {
-		case T_SELECT:
-			while (stack->size>0) {
-				tok = stack_rpop(stack);
-				if (tok->token == T_FROM)
-					break;
-			}
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
+	if (tok->token == T_SELECT) {
+		while (stack->size>0) {
 			tok = stack_rpop(stack);
-			if (tok->token != T_QUID) {
-				ERROR(ESQL_PARSE_VAL, EL_WARN);
-				return &rs;
-			}
-			rs.data = db_get(tok->string, len);
-			if (rs.data)
-				return &rs;
-			break;
-		case T_INSERT: {
-			struct objname {
-				char *name;
-				int length;
-			} *name = NULL;
-			schema_t schema = SCHEMA_ARRAY;
-			if (cnt == 1)
-				schema = SCHEMA_FIELD;
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token == T_TARGET) {
-				if (stack->size<=0) {
-					ERROR(ESQL_PARSE_END, EL_WARN);
-					return &rs;
-				}
-				tok = stack_rpop(stack);
-				if (tok->token == T_QUID) {
-				} else if (tok->token == T_NULL) {
-				} else {
-					ERROR(ESQL_PARSE_VAL, EL_WARN);
-					return &rs;
-				}
-				if (stack->size<=0) {
-					ERROR(ESQL_PARSE_END, EL_WARN);
-					return &rs;
-				}
-				tok = stack_rpop(stack);
-			}
-			if (tok->token == T_SEPARATE)
-				goto insert_arr;
-			if (tok->token != T_BRACK_OPEN) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-			schema = SCHEMA_OBJECT;
-			int name_cnt = cnt/2;
-			name = zmalloc(sizeof(struct objname) * name_cnt);
-			int i = 0;
-			while (stack->size>0) {
-				tok = stack_rpop(stack);
-				if (tok->token == T_BRACK_CLOSE)
-					break;
-				if (i+1 > name_cnt) {
-					name = zrealloc(name, sizeof(struct objname) * ++name_cnt);
-				}
-				if (tok->token == T_STRING) {
-					name[i].name = tok->string;
-					name[i].length = tok->length;
-					i++;
-					cnt--;
-				}
-			}
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token != T_SEPARATE) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-insert_arr:
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token != T_BRACK_OPEN) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-			size_t slay_len = 0;
-			int j = 0;
-			if (name && cnt > i)
-				cnt = i;
-			void *slay = create_row(schema, cnt, charcnt, &slay_len);
-			void *next = movetodata_row(slay);
-			while (stack->size>0) {
-				tok = stack_rpop(stack);
-				if (tok->token == T_BRACK_CLOSE)
-					break;
-				if (name && j>=i)
-					break;
-				if (tok->token == T_STRING) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_TEXT);
-					else
-						next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_TEXT);
-				} else if (tok->token == T_INTEGER || tok->token == T_DOUBLE) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_INT);
-					else
-						next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_INT);
-				} else if (tok->token == T_QUID) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_QUID);
-					else
-						next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_QUID);
-				} else if (tok->token == T_FALSE) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_F);
-					else
-						next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_F);
-				} else if (tok->token == T_TRUE) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_T);
-					else
-						next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_T);
-				}
-				j++;
-			}
-			if (name)
-				zfree(name);
-			_db_put(rs.quid, slay, slay_len);
-			rs.items = j;
+			if (tok->token == T_FROM)
+				break;
+		}
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
 			return &rs;
 		}
-		case T_UPDATE:
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token != T_QUID) {
-				ERROR(ESQL_PARSE_VAL, EL_WARN);
-				return &rs;
-			}
-			strcpy(rs.quid, tok->string);
-			struct objname {
-				char *name;
-				int length;
-			} *name = NULL;
-			schema_t schema = SCHEMA_ARRAY;
-			if (--cnt == 1)
-				schema = SCHEMA_FIELD;
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token != T_SEPARATE) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token == T_SEPARATE)
-				goto update_arr;
-			if (tok->token != T_BRACK_OPEN) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-			schema = SCHEMA_OBJECT;
-			int name_cnt = cnt/2;
-			name = zmalloc(sizeof(struct objname) * name_cnt);
-			int i = 0;
-			while (stack->size>0) {
-				tok = stack_rpop(stack);
-				if (tok->token == T_BRACK_CLOSE)
-					break;
-				if (i+1 > name_cnt) {
-					name = zrealloc(name, sizeof(struct objname) * ++name_cnt);
-				}
-				if (tok->token == T_STRING) {
-					name[i].name = tok->string;
-					name[i].length = tok->length;
-					i++;
-					cnt--;
-				}
-			}
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token != T_SEPARATE) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-update_arr:
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token != T_BRACK_OPEN) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-			size_t slay_len = 0;
-			int j = 0;
-			if (name && cnt > i)
-				cnt = i;
-			void *slay = create_row(schema, cnt, charcnt, &slay_len);
-			void *next = movetodata_row(slay);
-			while (stack->size>0) {
-				tok = stack_rpop(stack);
-				if (tok->token == T_BRACK_CLOSE)
-					break;
-				if (name && j>=i)
-					break;
-				if (tok->token == T_STRING) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_TEXT);
-					else
-						next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_TEXT);
-				} else if (tok->token == T_INTEGER || tok->token == T_DOUBLE) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_INT);
-					else
-						next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_INT);
-				} else if (tok->token == T_QUID) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_QUID);
-					else
-						next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_QUID);
-				} else if (tok->token == T_FALSE) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_F);
-					else
-						next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_F);
-				} else if (tok->token == T_TRUE) {
-					if (name)
-						next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_T);
-					else
-						next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_T);
-				}
-				j++;
-			}
-			if (name)
-				zfree(name);
-			_db_update(rs.quid, slay, slay_len);
-			rs.items = j;
+		tok = stack_rpop(stack);
+		if (tok->token != T_QUID) {
+			ERROR(ESQL_PARSE_VAL, EL_WARN);
 			return &rs;
-		case T_DELETE:
+		}
+		rs.data = db_get(tok->string, len);
+		if (rs.data)
+			return &rs;
+	} else if (tok->token == T_INSERT) {
+		struct objname {
+			char *name;
+			int length;
+		} *name = NULL;
+		schema_t schema = SCHEMA_ARRAY;
+		if (cnt == 1)
+			schema = SCHEMA_FIELD;
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token == T_TARGET) {
 			if (stack->size<=0) {
 				ERROR(ESQL_PARSE_END, EL_WARN);
 				return &rs;
 			}
 			tok = stack_rpop(stack);
-			if (tok->token != T_FROM) {
-				ERROR(ESQL_PARSE_TOK, EL_WARN);
-				return &rs;
-			}
-			if (stack->size<=0) {
-				ERROR(ESQL_PARSE_END, EL_WARN);
-				return &rs;
-			}
-			tok = stack_rpop(stack);
-			if (tok->token != T_QUID) {
+			if (tok->token == T_QUID) {
+			} else if (tok->token == T_NULL) {
+			} else {
 				ERROR(ESQL_PARSE_VAL, EL_WARN);
 				return &rs;
 			}
-			db_delete(tok->string);
-			break;
-		case T_ALL:
-		case T_FROM:
-		case T_WHERE:
-		case T_AND:
-		case T_OR:
-		case T_BRACK_OPEN:
-		case T_BRACK_CLOSE:
-		case T_SEPARATE:
-		case T_COMMIT:
-		case T_TARGET:
-		case T_GREATER:
-		case T_SMALLER:
-		case T_EQUAL:
-		case T_GREATER_EQUAL:
-		case T_SMALLER_EQUAL:
-		case T_INTEGER:
-		case T_DOUBLE:
-		case T_STRING:
-		case T_QUID:
-		case T_TRUE:
-		case T_FALSE:
-		case T_NULL:
-		case T_INVALID:
+			if (stack->size<=0) {
+				ERROR(ESQL_PARSE_END, EL_WARN);
+				return &rs;
+			}
+			tok = stack_rpop(stack);
+		}
+		if (tok->token == T_SEPARATE)
+			goto insert_arr;
+		if (tok->token != T_BRACK_OPEN) {
 			ERROR(ESQL_PARSE_TOK, EL_WARN);
-			break;
+			return &rs;
+		}
+		schema = SCHEMA_OBJECT;
+		int name_cnt = cnt/2;
+		name = zmalloc(sizeof(struct objname) * name_cnt);
+		int i = 0;
+		while (stack->size>0) {
+			tok = stack_rpop(stack);
+			if (tok->token == T_BRACK_CLOSE)
+				break;
+			if (i+1 > name_cnt) {
+				name = zrealloc(name, sizeof(struct objname) * ++name_cnt);
+			}
+			if (tok->token == T_STRING) {
+				name[i].name = tok->string;
+				name[i].length = tok->length;
+				i++;
+				cnt--;
+			}
+		}
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_SEPARATE) {
+			ERROR(ESQL_PARSE_TOK, EL_WARN);
+			return &rs;
+		}
+insert_arr:
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_BRACK_OPEN) {
+			ERROR(ESQL_PARSE_TOK, EL_WARN);
+			return &rs;
+		}
+		size_t slay_len = 0;
+		int j = 0;
+		if (name && cnt > i)
+			cnt = i;
+		void *slay = create_row(schema, cnt, charcnt, &slay_len);
+		void *next = movetodata_row(slay);
+		while (stack->size>0) {
+			tok = stack_rpop(stack);
+			if (tok->token == T_BRACK_CLOSE)
+				break;
+			if (name && j>=i)
+				break;
+			if (tok->token == T_STRING) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_TEXT);
+				else
+					next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_TEXT);
+			} else if (tok->token == T_INTEGER || tok->token == T_DOUBLE) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_INT);
+				else
+					next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_INT);
+			} else if (tok->token == T_QUID) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_QUID);
+				else
+					next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_QUID);
+			} else if (tok->token == T_FALSE) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_F);
+				else
+					next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_F);
+			} else if (tok->token == T_TRUE) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_T);
+				else
+					next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_T);
+			}
+			j++;
+		}
+		if (name)
+			zfree(name);
+		_db_put(rs.quid, slay, slay_len);
+		rs.items = j;
+		return &rs;
+	} else if (tok->token == T_UPDATE) {
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_QUID) {
+			ERROR(ESQL_PARSE_VAL, EL_WARN);
+			return &rs;
+		}
+		strcpy(rs.quid, tok->string);
+		struct objname {
+			char *name;
+			int length;
+		} *name = NULL;
+		schema_t schema = SCHEMA_ARRAY;
+		if (--cnt == 1)
+			schema = SCHEMA_FIELD;
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_SEPARATE) {
+			ERROR(ESQL_PARSE_TOK, EL_WARN);
+			return &rs;
+		}
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token == T_SEPARATE)
+			goto update_arr;
+		if (tok->token != T_BRACK_OPEN) {
+			ERROR(ESQL_PARSE_TOK, EL_WARN);
+			return &rs;
+		}
+		schema = SCHEMA_OBJECT;
+		int name_cnt = cnt/2;
+		name = zmalloc(sizeof(struct objname) * name_cnt);
+		int i = 0;
+		while (stack->size>0) {
+			tok = stack_rpop(stack);
+			if (tok->token == T_BRACK_CLOSE)
+				break;
+			if (i+1 > name_cnt) {
+				name = zrealloc(name, sizeof(struct objname) * ++name_cnt);
+			}
+			if (tok->token == T_STRING) {
+				name[i].name = tok->string;
+				name[i].length = tok->length;
+				i++;
+				cnt--;
+			}
+		}
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_SEPARATE) {
+			ERROR(ESQL_PARSE_TOK, EL_WARN);
+			return &rs;
+		}
+update_arr:
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_BRACK_OPEN) {
+			ERROR(ESQL_PARSE_TOK, EL_WARN);
+			return &rs;
+		}
+		size_t slay_len = 0;
+		int j = 0;
+		if (name && cnt > i)
+			cnt = i;
+		void *slay = create_row(schema, cnt, charcnt, &slay_len);
+		void *next = movetodata_row(slay);
+		while (stack->size>0) {
+			tok = stack_rpop(stack);
+			if (tok->token == T_BRACK_CLOSE)
+				break;
+			if (name && j>=i)
+				break;
+			if (tok->token == T_STRING) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_TEXT);
+				else
+					next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_TEXT);
+			} else if (tok->token == T_INTEGER || tok->token == T_DOUBLE) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_INT);
+				else
+					next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_INT);
+			} else if (tok->token == T_QUID) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, tok->string, tok->length, DT_QUID);
+				else
+					next = slay_wrap(next, NULL, 0, tok->string, tok->length, DT_QUID);
+			} else if (tok->token == T_FALSE) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_F);
+				else
+					next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_F);
+			} else if (tok->token == T_TRUE) {
+				if (name)
+					next = slay_wrap(next, name[j].name, name[j].length, NULL, 0, DT_BOOL_T);
+				else
+					next = slay_wrap(next, NULL, 0, NULL, 0, DT_BOOL_T);
+			}
+			j++;
+		}
+		if (name)
+			zfree(name);
+		_db_update(rs.quid, slay, slay_len);
+		rs.items = j;
+		return &rs;
+	} else if (tok->token == T_DELETE) {
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_FROM) {
+			ERROR(ESQL_PARSE_TOK, EL_WARN);
+			return &rs;
+		}
+		if (stack->size<=0) {
+			ERROR(ESQL_PARSE_END, EL_WARN);
+			return &rs;
+		}
+		tok = stack_rpop(stack);
+		if (tok->token != T_QUID) {
+			ERROR(ESQL_PARSE_VAL, EL_WARN);
+			return &rs;
+		}
+		db_delete(tok->string);
+	} else {
+		ERROR(ESQL_PARSE_TOK, EL_WARN);
 	}
 	return &rs;
 }
@@ -507,7 +495,7 @@ int tokenize(stack_t *stack, char sql[]) {
 		} else if (!strcmp(pch, ")")) {
 			tok->token = T_BRACK_CLOSE;
 		} else if (!strcmp(pch, "*")) {
-			tok->token = T_ALL;
+			tok->token = T_ASTERISK;
 		} else if (!strcmp(pch, "FROM") || !strcmp(pch, "from")) {
 			tok->token = T_FROM;
 		} else if (!strcmp(pch, "WHERE") || !strcmp(pch, "where")) {
@@ -516,6 +504,14 @@ int tokenize(stack_t *stack, char sql[]) {
 			tok->token = T_AND;
 		} else if (!strcmp(pch, "OR") || !strcmp(pch, "or") || !strcmp(pch, "||")) {
 			tok->token = T_OR;
+		} else if (!strcmp(pch, "+")) {
+			tok->token = T_AND;
+		} else if (!strcmp(pch, "-")) {
+			tok->token = T_SUBTRACT;
+		} else if (!strcmp(pch, "/")) {
+			tok->token = T_DEVIDE;
+		} else if (!strcmp(pch, "%")) {
+			tok->token = T_MODULO;
 		} else if (!strcmp(pch, ">")) {
 			tok->token = T_GREATER;
 		} else if (!strcmp(pch, "<")) {
@@ -526,6 +522,24 @@ int tokenize(stack_t *stack, char sql[]) {
 			tok->token =  T_SMALLER_EQUAL;
 		} else if (!strcmp(pch, ">=")) {
 			tok->token =  T_GREATER_EQUAL;
+		} else if (!strcmp(pch, "!=") || !strcmp(pch, "NOT") || !strcmp(pch, "not") || !strcmp(pch, "<>")) {
+			tok->token =  T_NOT;
+		} else if (!strcmp(pch, "ANY") || !strcmp(pch, "any")) {
+			tok->token =  T_ANY;
+		} else if (!strcmp(pch, "ALL") || !strcmp(pch, "all")) {
+			tok->token =  T_ALL;
+		} else if (!strcmp(pch, "BETWEEN") || !strcmp(pch, "between")) {
+			tok->token =  T_BETWEEN;
+		} else if (!strcmp(pch, "EXISTS") || !strcmp(pch, "exists")) {
+			tok->token =  T_EXISTS;
+		} else if (!strcmp(pch, "IN") || !strcmp(pch, "in")) {
+			tok->token =  T_IN;
+		} else if (!strcmp(pch, "LIKE") || !strcmp(pch, "like")) {
+			tok->token =  T_LIKE;
+		} else if (!strcmp(pch, "UNIQUE") || !strcmp(pch, "unique")) {
+			tok->token =  T_UNIQUE;
+		} else if (!strcmp(pch, "IS") || !strcmp(pch, "is")) {
+			tok->token =  T_IS;
 		} else if (!strcmp(pch, ";")) {
 			tok->token =  T_COMMIT;
 		} else if (!strcmp(pch, "TRUE") || !strcmp(pch, "true")) {
