fab407990095482fc8eb42c52c7f032f8ed3d430
yorickdewid <yorick17@outlook.com>
2015-07-01 11:53:46 +0200

    Faster CRC64 implementation

--- src/crc64.c
+++ src/crc64.c
@@ -1,144 +1,242 @@
-#include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <assert.h>
 
 #include <config.h>
 
-static const uint64_t crc64_tab[256] = {
-	UINT64_C(0x0000000000000000), UINT64_C(0x7ad870c830358979),
-	UINT64_C(0xf5b0e190606b12f2), UINT64_C(0x8f689158505e9b8b),
-	UINT64_C(0xc038e5739841b68f), UINT64_C(0xbae095bba8743ff6),
-	UINT64_C(0x358804e3f82aa47d), UINT64_C(0x4f50742bc81f2d04),
-	UINT64_C(0xab28ecb46814fe75), UINT64_C(0xd1f09c7c5821770c),
-	UINT64_C(0x5e980d24087fec87), UINT64_C(0x24407dec384a65fe),
-	UINT64_C(0x6b1009c7f05548fa), UINT64_C(0x11c8790fc060c183),
-	UINT64_C(0x9ea0e857903e5a08), UINT64_C(0xe478989fa00bd371),
-	UINT64_C(0x7d08ff3b88be6f81), UINT64_C(0x07d08ff3b88be6f8),
-	UINT64_C(0x88b81eabe8d57d73), UINT64_C(0xf2606e63d8e0f40a),
-	UINT64_C(0xbd301a4810ffd90e), UINT64_C(0xc7e86a8020ca5077),
-	UINT64_C(0x4880fbd87094cbfc), UINT64_C(0x32588b1040a14285),
-	UINT64_C(0xd620138fe0aa91f4), UINT64_C(0xacf86347d09f188d),
-	UINT64_C(0x2390f21f80c18306), UINT64_C(0x594882d7b0f40a7f),
-	UINT64_C(0x1618f6fc78eb277b), UINT64_C(0x6cc0863448deae02),
-	UINT64_C(0xe3a8176c18803589), UINT64_C(0x997067a428b5bcf0),
-	UINT64_C(0xfa11fe77117cdf02), UINT64_C(0x80c98ebf2149567b),
-	UINT64_C(0x0fa11fe77117cdf0), UINT64_C(0x75796f2f41224489),
-	UINT64_C(0x3a291b04893d698d), UINT64_C(0x40f16bccb908e0f4),
-	UINT64_C(0xcf99fa94e9567b7f), UINT64_C(0xb5418a5cd963f206),
-	UINT64_C(0x513912c379682177), UINT64_C(0x2be1620b495da80e),
-	UINT64_C(0xa489f35319033385), UINT64_C(0xde51839b2936bafc),
-	UINT64_C(0x9101f7b0e12997f8), UINT64_C(0xebd98778d11c1e81),
-	UINT64_C(0x64b116208142850a), UINT64_C(0x1e6966e8b1770c73),
-	UINT64_C(0x8719014c99c2b083), UINT64_C(0xfdc17184a9f739fa),
-	UINT64_C(0x72a9e0dcf9a9a271), UINT64_C(0x08719014c99c2b08),
-	UINT64_C(0x4721e43f0183060c), UINT64_C(0x3df994f731b68f75),
-	UINT64_C(0xb29105af61e814fe), UINT64_C(0xc849756751dd9d87),
-	UINT64_C(0x2c31edf8f1d64ef6), UINT64_C(0x56e99d30c1e3c78f),
-	UINT64_C(0xd9810c6891bd5c04), UINT64_C(0xa3597ca0a188d57d),
-	UINT64_C(0xec09088b6997f879), UINT64_C(0x96d1784359a27100),
-	UINT64_C(0x19b9e91b09fcea8b), UINT64_C(0x636199d339c963f2),
-	UINT64_C(0xdf7adabd7a6e2d6f), UINT64_C(0xa5a2aa754a5ba416),
-	UINT64_C(0x2aca3b2d1a053f9d), UINT64_C(0x50124be52a30b6e4),
-	UINT64_C(0x1f423fcee22f9be0), UINT64_C(0x659a4f06d21a1299),
-	UINT64_C(0xeaf2de5e82448912), UINT64_C(0x902aae96b271006b),
-	UINT64_C(0x74523609127ad31a), UINT64_C(0x0e8a46c1224f5a63),
-	UINT64_C(0x81e2d7997211c1e8), UINT64_C(0xfb3aa75142244891),
-	UINT64_C(0xb46ad37a8a3b6595), UINT64_C(0xceb2a3b2ba0eecec),
-	UINT64_C(0x41da32eaea507767), UINT64_C(0x3b024222da65fe1e),
-	UINT64_C(0xa2722586f2d042ee), UINT64_C(0xd8aa554ec2e5cb97),
-	UINT64_C(0x57c2c41692bb501c), UINT64_C(0x2d1ab4dea28ed965),
-	UINT64_C(0x624ac0f56a91f461), UINT64_C(0x1892b03d5aa47d18),
-	UINT64_C(0x97fa21650afae693), UINT64_C(0xed2251ad3acf6fea),
-	UINT64_C(0x095ac9329ac4bc9b), UINT64_C(0x7382b9faaaf135e2),
-	UINT64_C(0xfcea28a2faafae69), UINT64_C(0x8632586aca9a2710),
-	UINT64_C(0xc9622c4102850a14), UINT64_C(0xb3ba5c8932b0836d),
-	UINT64_C(0x3cd2cdd162ee18e6), UINT64_C(0x460abd1952db919f),
-	UINT64_C(0x256b24ca6b12f26d), UINT64_C(0x5fb354025b277b14),
-	UINT64_C(0xd0dbc55a0b79e09f), UINT64_C(0xaa03b5923b4c69e6),
-	UINT64_C(0xe553c1b9f35344e2), UINT64_C(0x9f8bb171c366cd9b),
-	UINT64_C(0x10e3202993385610), UINT64_C(0x6a3b50e1a30ddf69),
-	UINT64_C(0x8e43c87e03060c18), UINT64_C(0xf49bb8b633338561),
-	UINT64_C(0x7bf329ee636d1eea), UINT64_C(0x012b592653589793),
-	UINT64_C(0x4e7b2d0d9b47ba97), UINT64_C(0x34a35dc5ab7233ee),
-	UINT64_C(0xbbcbcc9dfb2ca865), UINT64_C(0xc113bc55cb19211c),
-	UINT64_C(0x5863dbf1e3ac9dec), UINT64_C(0x22bbab39d3991495),
-	UINT64_C(0xadd33a6183c78f1e), UINT64_C(0xd70b4aa9b3f20667),
-	UINT64_C(0x985b3e827bed2b63), UINT64_C(0xe2834e4a4bd8a21a),
-	UINT64_C(0x6debdf121b863991), UINT64_C(0x1733afda2bb3b0e8),
-	UINT64_C(0xf34b37458bb86399), UINT64_C(0x8993478dbb8deae0),
-	UINT64_C(0x06fbd6d5ebd3716b), UINT64_C(0x7c23a61ddbe6f812),
-	UINT64_C(0x3373d23613f9d516), UINT64_C(0x49aba2fe23cc5c6f),
-	UINT64_C(0xc6c333a67392c7e4), UINT64_C(0xbc1b436e43a74e9d),
-	UINT64_C(0x95ac9329ac4bc9b5), UINT64_C(0xef74e3e19c7e40cc),
-	UINT64_C(0x601c72b9cc20db47), UINT64_C(0x1ac40271fc15523e),
-	UINT64_C(0x5594765a340a7f3a), UINT64_C(0x2f4c0692043ff643),
-	UINT64_C(0xa02497ca54616dc8), UINT64_C(0xdafce7026454e4b1),
-	UINT64_C(0x3e847f9dc45f37c0), UINT64_C(0x445c0f55f46abeb9),
-	UINT64_C(0xcb349e0da4342532), UINT64_C(0xb1eceec59401ac4b),
-	UINT64_C(0xfebc9aee5c1e814f), UINT64_C(0x8464ea266c2b0836),
-	UINT64_C(0x0b0c7b7e3c7593bd), UINT64_C(0x71d40bb60c401ac4),
-	UINT64_C(0xe8a46c1224f5a634), UINT64_C(0x927c1cda14c02f4d),
-	UINT64_C(0x1d148d82449eb4c6), UINT64_C(0x67ccfd4a74ab3dbf),
-	UINT64_C(0x289c8961bcb410bb), UINT64_C(0x5244f9a98c8199c2),
-	UINT64_C(0xdd2c68f1dcdf0249), UINT64_C(0xa7f41839ecea8b30),
-	UINT64_C(0x438c80a64ce15841), UINT64_C(0x3954f06e7cd4d138),
-	UINT64_C(0xb63c61362c8a4ab3), UINT64_C(0xcce411fe1cbfc3ca),
-	UINT64_C(0x83b465d5d4a0eece), UINT64_C(0xf96c151de49567b7),
-	UINT64_C(0x76048445b4cbfc3c), UINT64_C(0x0cdcf48d84fe7545),
-	UINT64_C(0x6fbd6d5ebd3716b7), UINT64_C(0x15651d968d029fce),
-	UINT64_C(0x9a0d8ccedd5c0445), UINT64_C(0xe0d5fc06ed698d3c),
-	UINT64_C(0xaf85882d2576a038), UINT64_C(0xd55df8e515432941),
-	UINT64_C(0x5a3569bd451db2ca), UINT64_C(0x20ed197575283bb3),
-	UINT64_C(0xc49581ead523e8c2), UINT64_C(0xbe4df122e51661bb),
-	UINT64_C(0x3125607ab548fa30), UINT64_C(0x4bfd10b2857d7349),
-	UINT64_C(0x04ad64994d625e4d), UINT64_C(0x7e7514517d57d734),
-	UINT64_C(0xf11d85092d094cbf), UINT64_C(0x8bc5f5c11d3cc5c6),
-	UINT64_C(0x12b5926535897936), UINT64_C(0x686de2ad05bcf04f),
-	UINT64_C(0xe70573f555e26bc4), UINT64_C(0x9ddd033d65d7e2bd),
-	UINT64_C(0xd28d7716adc8cfb9), UINT64_C(0xa85507de9dfd46c0),
-	UINT64_C(0x273d9686cda3dd4b), UINT64_C(0x5de5e64efd965432),
-	UINT64_C(0xb99d7ed15d9d8743), UINT64_C(0xc3450e196da80e3a),
-	UINT64_C(0x4c2d9f413df695b1), UINT64_C(0x36f5ef890dc31cc8),
-	UINT64_C(0x79a59ba2c5dc31cc), UINT64_C(0x037deb6af5e9b8b5),
-	UINT64_C(0x8c157a32a5b7233e), UINT64_C(0xf6cd0afa9582aa47),
-	UINT64_C(0x4ad64994d625e4da), UINT64_C(0x300e395ce6106da3),
-	UINT64_C(0xbf66a804b64ef628), UINT64_C(0xc5bed8cc867b7f51),
-	UINT64_C(0x8aeeace74e645255), UINT64_C(0xf036dc2f7e51db2c),
-	UINT64_C(0x7f5e4d772e0f40a7), UINT64_C(0x05863dbf1e3ac9de),
-	UINT64_C(0xe1fea520be311aaf), UINT64_C(0x9b26d5e88e0493d6),
-	UINT64_C(0x144e44b0de5a085d), UINT64_C(0x6e963478ee6f8124),
-	UINT64_C(0x21c640532670ac20), UINT64_C(0x5b1e309b16452559),
-	UINT64_C(0xd476a1c3461bbed2), UINT64_C(0xaeaed10b762e37ab),
-	UINT64_C(0x37deb6af5e9b8b5b), UINT64_C(0x4d06c6676eae0222),
-	UINT64_C(0xc26e573f3ef099a9), UINT64_C(0xb8b627f70ec510d0),
-	UINT64_C(0xf7e653dcc6da3dd4), UINT64_C(0x8d3e2314f6efb4ad),
-	UINT64_C(0x0256b24ca6b12f26), UINT64_C(0x788ec2849684a65f),
-	UINT64_C(0x9cf65a1b368f752e), UINT64_C(0xe62e2ad306bafc57),
-	UINT64_C(0x6946bb8b56e467dc), UINT64_C(0x139ecb4366d1eea5),
-	UINT64_C(0x5ccebf68aecec3a1), UINT64_C(0x2616cfa09efb4ad8),
-	UINT64_C(0xa97e5ef8cea5d153), UINT64_C(0xd3a62e30fe90582a),
-	UINT64_C(0xb0c7b7e3c7593bd8), UINT64_C(0xca1fc72bf76cb2a1),
-	UINT64_C(0x45775673a732292a), UINT64_C(0x3faf26bb9707a053),
-	UINT64_C(0x70ff52905f188d57), UINT64_C(0x0a2722586f2d042e),
-	UINT64_C(0x854fb3003f739fa5), UINT64_C(0xff97c3c80f4616dc),
-	UINT64_C(0x1bef5b57af4dc5ad), UINT64_C(0x61372b9f9f784cd4),
-	UINT64_C(0xee5fbac7cf26d75f), UINT64_C(0x9487ca0fff135e26),
-	UINT64_C(0xdbd7be24370c7322), UINT64_C(0xa10fceec0739fa5b),
-	UINT64_C(0x2e675fb4576761d0), UINT64_C(0x54bf2f7c6752e8a9),
-	UINT64_C(0xcdcf48d84fe75459), UINT64_C(0xb71738107fd2dd20),
-	UINT64_C(0x387fa9482f8c46ab), UINT64_C(0x42a7d9801fb9cfd2),
-	UINT64_C(0x0df7adabd7a6e2d6), UINT64_C(0x772fdd63e7936baf),
-	UINT64_C(0xf8474c3bb7cdf024), UINT64_C(0x829f3cf387f8795d),
-	UINT64_C(0x66e7a46c27f3aa2c), UINT64_C(0x1c3fd4a417c62355),
-	UINT64_C(0x935745fc4798b8de), UINT64_C(0xe98f353477ad31a7),
-	UINT64_C(0xa6df411fbfb21ca3), UINT64_C(0xdc0731d78f8795da),
-	UINT64_C(0x536fa08fdfd90e51), UINT64_C(0x29b7d047efec8728),
-};
+/* 64-bit CRC polynomial with these coefficients, but reversed:
+	64, 62, 57, 55, 54, 53, 52, 47, 46, 45, 40, 39, 38, 37, 35, 33, 32,
+	31, 29, 27, 24, 23, 22, 21, 19, 17, 13, 12, 10, 9, 7, 4, 1, 0 */
+#define POLY UINT64_C(0xc96c5795d7870f42)
 
-uint64_t crc64(uint64_t crc, const unsigned char *s, uint64_t l) {
-	uint64_t j;
+/* Tables for CRC calculation -- filled in by initialization functions that are
+   called once.  These could be replaced by constant tables generated in the
+   same way.  There are two tables, one for each endianess.  Since these are
+   static, i.e. local, one should be compiled out of existence if the compiler
+   can evaluate the endianess check in crc64() at compile time. */
+static uint64_t crc64_little_table[8][256];
+static uint64_t crc64_big_table[8][256];
 
-	for (j=0; j<l; ++j) {
-		uint8_t byte = s[j];
-		crc = crc64_tab[(uint8_t)crc ^ byte] ^ (crc >> 8);
+#define GF2_DIM 64      /* dimension of GF(2) vectors (length of CRC) */
+
+/* Fill in the CRC-64 constants table. */
+static void crc64_init(uint64_t table[][256]) {
+	unsigned n, k;
+	uint64_t crc;
+
+	/* generate CRC-64's for all single byte sequences */
+	for (n=0; n<256; ++n) {
+		crc = n;
+		for (k=0; k<8; ++k)
+			crc = crc & 1 ? POLY ^ (crc >> 1) : crc >> 1;
+		table[0][n] = crc;
 	}
-	return crc;
+
+	/* generate CRC-64's for those followed by 1 to 7 zeros */
+	for (n=0; n<256; ++n) {
+		crc = table[0][n];
+		for (k=1; k<8; ++k) {
+			crc = table[0][crc & 0xff] ^ (crc >> 8);
+			table[k][n] = crc;
+		}
+	}
+}
+
+/* This function is called once to initialize the CRC-64 table for use on a
+   little-endian architecture. */
+static void crc64_little_init() {
+	crc64_init(crc64_little_table);
+}
+
+/* Reverse the bytes in a 64-bit word. */
+static inline uint64_t rev8(uint64_t a) {
+	uint64_t m;
+
+	m = UINT64_C(0xff00ff00ff00ff);
+	a = ((a >> 8) & m) | (a & m) << 8;
+	m = UINT64_C(0xffff0000ffff);
+	a = ((a >> 16) & m) | (a & m) << 16;
+	return a >> 32 | a << 32;
+}
+
+/* This function is called once to initialize the CRC-64 table for use on a
+   big-endian architecture. */
+static void crc64_big_init() {
+	unsigned k, n;
+
+	crc64_init(crc64_big_table);
+	for (k=0; k<8; ++k)
+		for (n=0; n<256; ++n)
+			crc64_big_table[k][n] = rev8(crc64_big_table[k][n]);
+}
+
+/* Run the init() function exactly once.  If pthread.h is not included, then
+   this macro will use a simple static state variable for the purpose, which is
+   not thread-safe.  The init function must be of the type void init(void). */
+#ifdef PTHREAD_ONCE_INIT
+#  define ONCE(init) \
+	do { \
+		static pthread_once_t once = PTHREAD_ONCE_INIT; \
+		pthread_once(&once, init); \
+	} while (0)
+#else
+#  define ONCE(init) \
+	do { \
+		static volatile int once = 1; \
+		if (once) { \
+			if (once++ == 1) { \
+				init(); \
+				once = 0; \
+			} \
+			else \
+				while (once) \
+					; \
+		} \
+	} while (0)
+#endif
+
+/* Calculate a CRC-64 eight bytes at a time on a little-endian architecture. */
+static inline uint64_t crc64_little(uint64_t crc, void *buf, size_t len) {
+	unsigned char *next = buf;
+
+	ONCE(crc64_little_init);
+	crc = ~crc;
+	while (len && ((uintptr_t)next & 7) != 0) {
+		crc = crc64_little_table[0][(crc ^ *next++) & 0xff] ^ (crc >> 8);
+		len--;
+	}
+	while (len >= 8) {
+		crc ^= *(uint64_t *)next;
+		crc = crc64_little_table[7][crc & 0xff] ^
+			  crc64_little_table[6][(crc >> 8) & 0xff] ^
+			  crc64_little_table[5][(crc >> 16) & 0xff] ^
+			  crc64_little_table[4][(crc >> 24) & 0xff] ^
+			  crc64_little_table[3][(crc >> 32) & 0xff] ^
+			  crc64_little_table[2][(crc >> 40) & 0xff] ^
+			  crc64_little_table[1][(crc >> 48) & 0xff] ^
+			  crc64_little_table[0][crc >> 56];
+		next += 8;
+		len -= 8;
+	}
+	while (len) {
+		crc = crc64_little_table[0][(crc ^ *next++) & 0xff] ^ (crc >> 8);
+		len--;
+	}
+	return ~crc;
+}
+
+/* Calculate a CRC-64 eight bytes at a time on a big-endian architecture. */
+static inline uint64_t crc64_big(uint64_t crc, void *buf, size_t len) {
+	unsigned char *next = buf;
+
+	ONCE(crc64_big_init);
+	crc = ~rev8(crc);
+	while (len && ((uintptr_t)next & 7) != 0) {
+		crc = crc64_big_table[0][(crc >> 56) ^ *next++] ^ (crc << 8);
+		len--;
+	}
+	while (len >= 8) {
+		crc ^= *(uint64_t *)next;
+		crc = crc64_big_table[0][crc & 0xff] ^
+			  crc64_big_table[1][(crc >> 8) & 0xff] ^
+			  crc64_big_table[2][(crc >> 16) & 0xff] ^
+			  crc64_big_table[3][(crc >> 24) & 0xff] ^
+			  crc64_big_table[4][(crc >> 32) & 0xff] ^
+			  crc64_big_table[5][(crc >> 40) & 0xff] ^
+			  crc64_big_table[6][(crc >> 48) & 0xff] ^
+			  crc64_big_table[7][crc >> 56];
+		next += 8;
+		len -= 8;
+	}
+	while (len) {
+		crc = crc64_big_table[0][(crc >> 56) ^ *next++] ^ (crc << 8);
+		len--;
+	}
+	return ~rev8(crc);
+}
+
+/* Return the CRC-64 of buf[0..len-1] with initial crc, processing eight bytes
+   at a time.  This selects one of two routines depending on the endianess of
+   the architecture.  A good optimizing compiler will determine the endianess
+   at compile time if it can, and get rid of the unused code and table.  If the
+   endianess can be changed at run time, then this code will handle that as
+   well, initializing and using two tables, if called upon to do so. */
+uint64_t crc64(uint64_t crc, void *buf, size_t len) {
+	uint64_t n = 1;
+
+	return *(char *)&n ? crc64_little(crc, buf, len) : crc64_big(crc, buf, len);
+}
+
+static uint64_t gf2_matrix_times(uint64_t *mat, uint64_t vec) {
+	uint64_t sum;
+
+	sum = 0;
+	while (vec) {
+		if (vec & 1)
+			sum ^= *mat;
+		vec >>= 1;
+		mat++;
+	}
+	return sum;
+}
+
+static void gf2_matrix_square(uint64_t *square, uint64_t *mat) {
+	unsigned n;
+
+	for (n=0; n<GF2_DIM; ++n)
+		square[n] = gf2_matrix_times(mat, mat[n]);
+}
+
+/* Return the CRC-64 of two sequential blocks, where crc1 is the CRC-64 of the
+   first block, crc2 is the CRC-64 of the second block, and len2 is the length
+   of the second block. */
+uint64_t crc64_combine(uint64_t crc1, uint64_t crc2, uintmax_t len2) {
+	unsigned n;
+	uint64_t row;
+	uint64_t even[GF2_DIM];     /* even-power-of-two zeros operator */
+	uint64_t odd[GF2_DIM];      /* odd-power-of-two zeros operator */
+
+	/* degenerate case */
+	if (len2 == 0)
+		return crc1;
+
+	/* put operator for one zero bit in odd */
+	odd[0] = POLY;              /* CRC-64 polynomial */
+	row = 1;
+	for (n=1; n<GF2_DIM; ++n) {
+		odd[n] = row;
+		row <<= 1;
+	}
+
+	/* put operator for two zero bits in even */
+	gf2_matrix_square(even, odd);
+
+	/* put operator for four zero bits in odd */
+	gf2_matrix_square(odd, even);
+
+	/* apply len2 zeros to crc1 (first square will put the operator for one
+	   zero byte, eight zero bits, in even) */
+	do {
+		/* apply zeros operator for this bit of len2 */
+		gf2_matrix_square(even, odd);
+		if (len2 & 1)
+			crc1 = gf2_matrix_times(even, crc1);
+		len2 >>= 1;
+
+		/* if no more bits set, then done */
+		if (len2 == 0)
+			break;
+
+		/* another iteration of the loop with odd and even swapped */
+		gf2_matrix_square(odd, even);
+		if (len2 & 1)
+			crc1 = gf2_matrix_times(odd, crc1);
+		len2 >>= 1;
+
+		/* if no more bits set, then done */
+	} while (len2 != 0);
+
+	/* return combined crc */
+	crc1 ^= crc2;
+	return crc1;
 }
--- src/crc64.h
+++ src/crc64.h
@@ -1,6 +1,6 @@
 #ifndef CRC64_H_INCLUDED
 #define CRC64_H_INCLUDED
 
-uint64_t crc64(uint64_t crc, const unsigned char *s, uint64_t l);
+uint64_t crc64(uint64_t crc, void *buf, size_t len)
 
 #endif // CRC64_H_INCLUDED
