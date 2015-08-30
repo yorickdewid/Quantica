5cdc9c00948dc05a99e65894a627d5e7fa198cf1
yorickdewid <yorick17@outlook.com>
2015-08-30 17:33:21 -0400

    Check for return

--- test/dupquid.sh
+++ test/dupquid.sh
@@ -1,3 +1,8 @@
-../bin/genquid 2000 > dupchk
-sort dupchk | uniq -c -d
+#!/bin/bash
+COUNT=200000
+../bin/genquid $COUNT > dupchk
+if [[ -n $(sort dupchk | uniq -c -d) ]]; then
+  echo "Duplicate keys found"
+  exit 1
+fi
 rm -rf dupchk
