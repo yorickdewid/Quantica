028ae3c2875d7792443269509b4b2bbce73450a0
yorickdewid <yorick17@outlook.com>
2015-09-01 09:35:24 -0400

    Add testscripts

--- test/dupquid.sh
+++ test/dupquid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COUNT=200000
-../bin/genquid $COUNT > dupchk
+bin/genquid $COUNT > dupchk
 if [[ -n $(cut -f 2 -d ' ' dupchk | sort | uniq -c -d) ]]; then
   echo "Duplicate keys found"
   exit 1
