0c27297e9c1f49a5171c14c793738f784d481e7c
yorickdewid <yorick17@outlook.com>
2015-10-24 20:23:22 +0200

    Rename modules

--- src/marshall.h
+++ src/marshall.h
@@ -4,13 +4,13 @@
 #define marshall_free(v) tree_zfree(v);
 
 typedef enum {
-	DSTYPE_NULL,
-	DSTYPE_TRUE,
-	DICT_FALSE,
-	DICT_INT,
-	DICT_STR,
-	DICT_ARR,
-	DICT_OBJ
+	MTYPE_NULL,
+	MTYPE_TRUE,
+	MTYPE_FALSE,
+	MTYPE_INT,
+	MTYPE_STRING,
+	MTYPE_ARRAY,
+	MTYPE_OBJECT
 } serialize_type_t;
 
 typedef struct serialize {
@@ -20,6 +20,12 @@ typedef struct serialize {
 	unsigned int sz;
 	serialize_type_t type;
 } serialize_t;
+
+typedef struct {
+	schema_t schema;
+	size_t size;
+	serialize_t *data;
+} marshall_t;
 
 serialize_t *marshall_decode(char *data, size_t data_len, char *name, void *parent);
 char *marshall_encode(serialize_t *obj);
