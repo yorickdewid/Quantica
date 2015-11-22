9bee1ca960481b5ae7628e9722a0eef87c675ca0
yorickdewid <yorick17@outlook.com>
2015-11-22 22:24:11 +0100

    Fix RT

--- src/fdm.c
+++ src/fdm.c
@@ -20,7 +20,8 @@ double floor(double x) {
 			}
 		} else {
 			i = (0x000fffff) >> j0;
-			if (((i0 & i) | i1) == 0) return x; /* x is integral */
+			if (((i0 & i) | i1) == 0)
+				return x; /* x is integral */
 			if (huge + x > 0.0) {	/* raise inexact flag */
 				if (i0 < 0) i0 += (0x00100000) >> j0;
 				i0 &= (~i); i1 = 0;
